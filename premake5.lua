6449aa75ff493fe0609bbd55422b1b4f902789be
Jong Hoon Lee <j.h.lee2@newcastle.ac.uk>
2019-07-02 00:50:01 +0100

    updated .gitignore and premake file

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
     kind "StaticLib"
     language "C"
     
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("Build/" .. outputdir .. "/%{prj.name}")
+	objdir ("Build/internal/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
